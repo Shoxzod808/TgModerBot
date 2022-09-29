f721366fe6be12176da26a63a198a3dd91b5aa11
Shoxzod808 <pkzoom94320@gmail.com>
2022-09-29 23:44:55 +0500

    a

--- app/backend/models.py
+++ app/backend/models.py
@@ -36,15 +36,30 @@ class Template(models.Model):
     def __str__(self) -> str:
         return self.title
 
+class Template2Button(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Названия текста')
+    body_ru = models.TextField(verbose_name='Русский текст')
+    body_eng = models.TextField(verbose_name='Английский текст')
+
+    class Meta:
+         verbose_name = "Шаблон для кнопки"
+         verbose_name_plural = 'Шаблоны для кнопок'
+
+
+    def __str__(self) -> str:
+        return self.title
 
 class Group(models.Model):
-    name = models.CharField(max_length=255)
-    link = models.CharField(max_length=255, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    users_count = models.IntegerField(null=True, blank=True)
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255, default='-')
+    username = models.CharField(max_length=255, default='-')
+    description = models.TextField(default='-')
+    users_count = models.IntegerField(default=0)
     chat_id = models.IntegerField()
-    black_list = models.TextField(null=True, blank=True)
-    white_list = models.TextField(null=True, blank=True)
+    black_list = models.TextField(default='-')
+    white_list = models.TextField(default='-')
+    user = models.ForeignKey(to=BotUser, on_delete=models.CASCADE)
+    
 
     def __str__(self) -> str:
         return self.name
