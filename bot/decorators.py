4e97ad42aa17ab3210b3d8e69e382ab9f1f95c60
Shoxzod808 <pkzoom94320@gmail.com>
2022-10-01 00:09:26 +0500

    Update decorators.py

--- bot/decorators.py
+++ bot/decorators.py
@@ -1,5 +1,7 @@
+from typing import get_origin
 from aiogram.types import Message
 from asgiref.sync import sync_to_async
+import contextlib
 
 import config
 from backend.models import BotUser, Template, Template2Button, Group
@@ -17,10 +19,8 @@ def add_user(message: Message) -> None:
     if success:
         full_name = ' '.join([message.from_user.first_name, message.from_user.last_name or ''])
         user.full_name = full_name
-        try:
+        with contextlib.suppress(Exception):
             user.username = message.from_user.username
-        except Exception:
-            pass
         user.save()
 
 @sync_to_async
@@ -29,8 +29,7 @@ def is_bot(message: Message) -> bool:
 
 @sync_to_async
 def exists(chat_id) -> bool: 
-    user = BotUser.objects.filter(chat_id=chat_id).exists()
-    return user
+    return BotUser.objects.filter(chat_id=chat_id).exists()
 
 @sync_to_async
 def get_text(title, chat_id, button=False):
@@ -39,9 +38,7 @@ def get_text(title, chat_id, button=False):
     else:
         text = Template.objects.get(title=title)
     lang = BotUser.objects.get(chat_id=chat_id).language
-    if lang == 'ru':
-        return text.body_ru
-    return text.body_eng
+    return text.body_ru if lang == 'ru' else text.body_eng
 
 @sync_to_async
 def get_user_lang(chat_id):
@@ -57,16 +54,28 @@ def set_user_language(chat_id, lang):
 @sync_to_async
 def create_group(title, username, link, description, members_count, chat_id, chanel_chat_id, type):
     user = BotUser.objects.get(chat_id=chat_id)
-    group = Group.objects.create(
+    return Group.objects.create(
         chat_id=chanel_chat_id,
         title=title,
-        username=username,
-        link=link,
-        description=description,
-        users_count=members_count,
-        black_list='-',
-        white_list='-',
-        user=user,
+        username=username, 
+        link=link, 
+        description=description, 
+        users_count=members_count, 
+        black_list='-', 
+        white_list='-', 
+        user=user, 
         type=type
-    )
-    return group
+        )
+
+@sync_to_async
+def get_list_chats(chat_id) -> list:
+    buttons = []
+    user = BotUser.objects.filter(chat_id=chat_id)
+    if user.exists():
+        user = user[0]
+        buttons.extend(list(Group.objects.filter(user=user)))
+    return buttons
+
+@sync_to_async
+def get_group(id):
+    return Group.objects.get(id=id)
